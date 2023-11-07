ccb0b66f6fb727f13e0150b964e42fcf0d35d8da
Aman kohare <amankohare@gmail.com>
2023-11-07 19:44:44 +0530

    done everything

--- README.md
+++ README.md
@@ -1,7 +1,1 @@
-# mern-stack-blog-app
-complete mern stack blog application source code 2023
-
-# please check branches more code coming soon !
-Dont forgot follow on github as well 
-##  thanks for watching techinfoyt
-"# Full-Stack-Blog-Website" 
+# full-stack-blog-app
--- client/src/pages/UserBlogs.js
+++ client/src/pages/UserBlogs.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
 import BlogCard from "../components/BlogCard";
+import { Link } from "react-router-dom";
 const UserBlogs = () => {
   const [blogs, setBlogs] = useState([]);
 
@@ -23,22 +24,40 @@ const UserBlogs = () => {
   console.log(blogs);
   return (
     <div>
-      {blogs && blogs.length > 0 ? (
-        blogs.map((blog) => (
-          <BlogCard
-            id={blog._id}
-            isUser={true}
-            title={blog.title}
-            description={blog.description}
-            image={blog.image}
-            username={blog.user.username}
-            time={blog.createdAt}
-          />
-        ))
-      ) : (
-        <h1>You Havent Created a blog</h1>
-      )}
-    </div>
+    {blogs && blogs.length > 0 ? (
+      blogs.map((blog) => (
+        <BlogCard
+          id={blog._id}
+          isUser={true}
+          title={blog.title}
+          description={blog.description}
+          image={blog.image}
+          username={blog.user.username}
+          time={blog.createdAt}
+        />
+      ))
+    ) : (
+      <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'center', height: '100vh' }}>
+  <h1 style={{ color: 'white' }}>You Haven't Created a Blog</h1>
+  <button style={{
+    background: 'linear-gradient(to right, red, black)', // Linear gradient from red to black
+    color: 'white',
+    border: 'none',
+    padding: '10px 20px',
+    borderRadius: '5px',
+    cursor: 'pointer',
+    marginTop: '20px', // Add some top margin to separate the h1 and the button
+  }}>
+    <Link to="/create-blog" style={{ textDecoration: 'none', color: 'white' }}>
+      Create you first Blog
+    </Link>
+  </button>
+</div>
+
+
+    )}
+  </div>
+  
   );
 };
 
